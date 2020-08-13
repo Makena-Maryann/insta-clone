ece766e867600f1e99ceec39df0695f885d56003
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-13 05:35:03 +0300

    Create profile and new post view functions

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
-from .models import Image
+from .models import Image,Profile
+from .forms import NewPostForm,UpdateProfileForm
 
 # Create your views here.
 def index(request):
     posts = Image.get_images()
     return render(request, 'posts/index.html',{"posts":posts})
+
+def profile(request,prof_id):
+    photos = Image.filter_images_by_profile(prof_id)
+    profile = Profile.objects.get(id = prof_id)
+    
+    return render(request, 'posts/profile.html',{"profile":profile,"photos":photos})    
 
 def search_results(request):
 
@@ -28,3 +35,17 @@ def image(request,image_id):
     except DoesNotExist:
         raise Http404()
     return render(request,"posts/image.html", {"image":image})    
+
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.profile.owner = current_user
+            post.save()
+        return redirect('profile')
+
+    else:
+        form = NewPostForm()
+    return render(request,'new_post.html', {"form": form})                
