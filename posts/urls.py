8739dabc27b3cd5ec54f4a6bd18dc41910a59c9d
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-13 03:09:35 +0300

    Create search and single image url patterns

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    # url(r'^$',views.index,name = 'index'),
+    url(r'^$',views.index,name = 'index'),
+     url(r'^search/', views.search_results, name='search_results'),
+     url(r'^image/(\d+)',views.image,name='image')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
